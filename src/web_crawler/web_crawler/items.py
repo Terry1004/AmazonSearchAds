1a6694a8b7c83a75684cb18aeaa450d11142cfbd
wangyiranamy <yw3201@columbia.edu>
2018-10-03 14:28:14 -0400

    feeds data

--- src/web_crawler/web_crawler/items.py
+++ src/web_crawler/web_crawler/items.py
@@ -7,6 +7,31 @@
 
 import scrapy
 
+class AdCrawled(scrapy.Item):
+    # Attribute from feed
+    adId = scrapy.Field()
+    campaignId = scrapy.Field()
+    bidPrice = scrapy.Field()
+    query_group_id = scrapy.Field()
+    query = scrapy.Field()
+
+    # Attributes Crawled
+    title = scrapy.Field()
+    price = scrapy.Field()
+    thumbnail = scrapy.Field()
+    description = scrapy.Field()
+    brand = scrapy.Field()
+    detail_url = scrapy.Field()
+    category = scrapy.Field()
+
+    # Attribute Learned
+    keyWords = scrapy.Field()
+    relevanceScore = scrapy.Field()
+    pClick = scrapy.Field()
+    rankScore = scrapy.Field()
+    qualityScore = scrapy.Field()
+    costPerClick = scrapy.Field()
+    position = scrapy.Field() #1: top , 2: bottom
 
 class WebCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
