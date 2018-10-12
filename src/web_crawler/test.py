bccbbb119486771a4899e974044cf7c4a3a8ad40
wangyiranamy <yw3201@columbia.edu>
2018-10-12 15:08:46 -0400

    feeds added

--- src/web_crawler/test.py
+++ src/web_crawler/test.py
@@ -1,8 +1,11 @@
 from web_crawler.init.init_proxy import init_proxy
 from web_crawler.init.init_query import init_query
 
-for proxy in init_proxy():
-    print(proxy.proxy_address)
+# for proxy in init_proxy():
+#     print(proxy.proxy_address)
+count =0
+for query in init_query():
+    count+=1
+    print (query)
 
-# for query in init_query():
-#     print (query)
+print (count)
