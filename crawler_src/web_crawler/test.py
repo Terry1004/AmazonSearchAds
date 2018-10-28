dc158025004616d4967c692ec86eefc0d3410ba0
Terry1004 <hym961004@gmail.com>
2018-10-28 12:59:40 -0400

    Remove some file contents and will add these file to .gitignore

--- crawler_src/web_crawler/test.py
+++ crawler_src/web_crawler/test.py
@@ -1,14 +1,1 @@
 """ A simple test module for debugging """
-
-
-from web_crawler.init.init_proxy import init_proxy
-from web_crawler.init.init_query import init_query
-
-# for proxy in init_proxy():
-#     print(proxy.proxy_address)
-count =0
-for query in init_query():
-    count+=1
-    print (query)
-
-print (count)
