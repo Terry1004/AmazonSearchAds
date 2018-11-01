3009f8299e1358492451aadd396255e290309224
Terry1004 <hym961004@gmail.com>
2018-10-31 23:13:23 -0400

    Implement Ad and Compaign classes

--- web_src/src/io/amazon/ads/StaticObjs/Ad.java
+++ web_src/src/io/amazon/ads/StaticObjs/Ad.java
@@ -1,11 +1,32 @@
 package io.amazon.ads.StaticObjs;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * This is a class storing one ad information
+ * This class represents an ad with fields the same as stored in database
  */
-public class Ad {
+public class Ad implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
 	public String title = "";
+	public Long adId;
+	public Long campaignId;
+	public List<String> keyWords = new ArrayList<>();
+	public double relevanceScore;
+	public double pClick;	
+	public double bidPrice;
+	public double rankScore;
+	public double qualityScore;
+	public double costPerClick;
+	public int position = 1; //1: top , 2: bottom
+    public double price; 
+    public String thumbnail = ""; 
+    public String brand = ""; 
+    public String detail_url = ""; 
+    public String query = ""; 
+    public String category = "";
 	
 	public Ad(String title) {
 		this.title = title;
--- web_src/src/io/amazon/ads/StaticObjs/Campaign.java
+++ web_src/src/io/amazon/ads/StaticObjs/Campaign.java
@@ -1,5 +1,10 @@
 package io.amazon.ads.StaticObjs;
 
+/**
+ * This class represents a campaign with fields the same as stored in database
+ */
 public class Campaign {
-
+	
+	public Long campaignId;
+	public double budget;
 }
